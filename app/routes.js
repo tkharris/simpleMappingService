0c35ebfa482122f91d0b57b6361ae228cb1f0744
Thomas Harris <thomas@webkite.com>
2016-11-10 20:23:28 -0500

    adding app calls for passport

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,58 @@ var User            = require('./model.js');
 // Opens App Routes
 module.exports = function(app) {
 
+     // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.sendfile('public/views/index.html'); // load the index.ejs file
+    });
+
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.sendfile('public/views/login.html'); //, { message: req.flash('loginMessage') }); 
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.sendfile('public/views/signup.html'); //, { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.sendfile('public/views/profile.html'); //, {
+        //    user : req.user // get the user out of session and pass to template
+        //});
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
     // GET Routes
     // --------------------------------------------------------
     // Retrieve records for all users in the db
@@ -38,4 +90,15 @@ module.exports = function(app) {
             res.json(req.body);
         });
     });
+
+    // route middleware to make sure a user is logged in
+    function isLoggedIn(req, res, next) {
+
+        // if user is authenticated in the session, carry on 
+        if (req.isAuthenticated())
+            return next();
+
+        // if they aren't redirect them to the home page
+        res.redirect('/');
+    }
 };
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var port            = process.env.PORT || 3000;
 var morgan          = require('morgan');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
+var passport        = require('passport');
+var flash           = require('connect-flash');
+var cookieParser    = require('cookie-parser');
+var session         = require('express-session');
 var app             = express();
 
 // Express Configuration
@@ -13,19 +17,28 @@ var app             = express();
 // Sets the connection to MongoDB
 mongoose.connect("mongodb://localhost/MeanMapApp");
 
+// require('./config/passport')(passport); // pass passport for configuration
+
 // Logging and Parsing
 app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
 app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
 app.use(morgan('dev'));                                         // log with Morgan
+app.use(cookieParser());                                        // read cookies (needed for auth)
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
 app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 // Routes
 // ------------------------------------------------------
-require('./app/routes.js')(app);
+require('./app/routes.js')(app, passport);
 
 // Listen
 // -------------------------------------------------------
